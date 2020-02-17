63f247d88d2b9b6112b8a46ba920e18cd5e3fe72
eoang <efimoneangular@gmail.com>
2020-02-17 11:35:38 +0200

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name1 = 'Angular22';
 }
---
+++ src/app/models/user-model.ts
@@ -0,0 +1,21 @@
+export class UserModel {
+    constructor( id: number,
+    username: string,
+    email: string,
+    info: UserInfo)
+    {}
+    
+    
+    
+  }
+  class UserInfo
+  {
+    onstructor( 
+      firstName: string,
+      lastName: string,
+      jobTitle: string,
+      avatar: string,
+      phoneNumber: string
+   )
+   {}
+  }
