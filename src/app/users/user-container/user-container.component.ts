551bb8b04e265490ea834105aa175beb34927a34
eoang <efimoneangular@gmail.com>
2020-02-17 20:41:45 +0200

    3 files modified

--- src/app/users/user-container/user-container.component.ts
+++ src/app/users/user-container/user-container.component.ts
@@ -1,33 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 //import { UserModel, OnInit } from '@angular/core';
-import {UserInfo, UserModel} from '../../models/user-model';
+import { UserInfo, UserModel } from "../../models/user-model";
 @Component({
-  selector: 'app-user-container',
-  templateUrl: './user-container.component.html',
-  styleUrls: ['./user-container.component.css']
+  selector: "app-user-container",
+  templateUrl: "./user-container.component.html",
+  styleUrls: ["./user-container.component.css"]
 })
 export class UserContainerComponent implements OnInit {
-
-  currentUser : UserModel
-  constructor() { 
-     
-  }
+  currentUser: UserModel;
+  constructor() {}
 
   ngOnInit() {
-    console.log('User Container Init')
+    console.log("User Container Init");
 
-
-//console.log(this.currentUser.id);
-     this.currentUser = new UserModel(1,"Gah","Gah_don@gmail.com", 
-     new UserInfo ('Gah','don','Developer','http://authenticgoods.avatar-male.png') );
-
-
-  function Update()
-  {}
-     
+    //console.log(this.currentUser.id);
+    this.currentUser = new UserModel(
+      1,
+      "Gah",
+      "Gah_don@gmail.com",
+      new UserInfo(
+        "Gah",
+        "don",
+        "Developer",
+        "http://authenticgoods.co/wrapbootstrap/themes/sparks/img/team/avatar-male.png"
+      )
+    );
   }
-
-  
-
-   
+   updateUser(username: string, email: string){
+    this.currentUser.email = email;
+    this.currentUser.username = username;
+  }
 }
